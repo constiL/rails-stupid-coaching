4cec905a0b788da5c6c2a48eb018a68e88d8c00d
Constantin Lerner <colerner@me.com>
2023-11-20 16:14:43 +0100

    Stupid Coaching with some css applied

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,12 +7,3 @@ class QuestionsController < ApplicationController
     @answer = 'Great!' if params[:question] == 'I am going to work'
   end
 end
-
-
-# if params[:question] == "I am going to work"
-#   @answer = 'Great!'
-# elsif params[:question].end_with?('?')
-#   @answer = 'Silly question, get dressed and go to work!'
-# else
-#   @answer = "I don't care, get dressed and go to work!"
-# end
