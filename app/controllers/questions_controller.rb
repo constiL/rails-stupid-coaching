6474fedb8c94d916288914b723ecabe3e40c6d8e
Constantin Lerner <colerner@me.com>
2023-11-20 15:08:07 +0100

    changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,13 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = []
-    if params[:question] == "I am going to work"
-      @answer = 'Great!'
-    elsif params[:question].end_with?("?")
-      @answer = 'Silly question, get dressed and go to work!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = params[:question].end_with?('?') ?  'Silly question, get dressed and go to work!' : "I don't care, get dressed and go to work!" # rubocop:disable Layout/LineLength
+    @answer = 'Great!' if params[:question] == 'I am going to work'
   end
 end
+
+
+# if params[:question] == "I am going to work"
+#   @answer = 'Great!'
+# elsif params[:question].end_with?('?')
+#   @answer = 'Silly question, get dressed and go to work!'
+# else
+#   @answer = "I don't care, get dressed and go to work!"
+# end
